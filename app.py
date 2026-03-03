e299afdba512d6062f5238845e4e22e90cf9de05
DorkNet <enochnumbi062@gmail.com>
2026-03-03 19:46:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Désactiver le cache pour que vos changements sur engine.js soient instantanés
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route('/')
 def index():
-    # Flask va chercher automatiquement dans le dossier 'templates'
+    """
+    Sert la page principale du jeu. 
+    Flask cherche automatiquement le fichier 'index.html' dans le dossier /templates.
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Utilisation du port fourni par Render ou 5000 par défaut
+    # Configuration vitale pour le déploiement :
+    # Render attribue un PORT dynamiquement, nous devons le récupérer.
     port = int(os.environ.get("PORT", 5000))
+    
+    # host='0.0.0.0' permet au serveur d'être visible sur le web
     app.run(host='0.0.0.0', port=port)
